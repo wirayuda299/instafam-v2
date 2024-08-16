555b6831eaeda6c849742eef1d7e8999417c35bc
wira233 <wirayuda233@gmail.com>
2024-08-16 11:17:45 -0700

    fix unused char

--- client/app/(main)/page.tsx
+++ client/app/(main)/page.tsx
@@ -14,12 +14,12 @@ export default async function Home() {
   return (
     <main className="no-scrollbar flex h-full" id="test">
       <div className="flex h-auto max-h-screen w-full flex-col gap-5 divide-y divide-gray-600/50 overflow-y-auto p-2 max-md:max-h-dvh">
-        {posts?.map((post?) => (
+        {posts?.map((post) => (
           <PostCard
             fileId={post?.media_asset_id}
             createdAt={post?.createdat}
             authorId={post?.author_id}
-            likes={post?.likes}
+            likes={post?.likes ||[]}
             priority={true}
             loading="eager"
             postId={post?.post_id}
