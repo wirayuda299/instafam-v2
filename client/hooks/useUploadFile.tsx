91028fd63f576ae784f5d09106595a852bec2c66
wirayuda233 <wirayuda233@gmail.com>
2024-08-23 15:56:54 +0000

    refactor

--- client/hooks/useUploadFile.tsx
+++ client/hooks/useUploadFile.tsx
@@ -1,82 +1,63 @@
-import {
-  encodeImageToBlurhash,
-  getImageData,
-  loadImage,
-} from "@/utils/image-loader";
 import { ChangeEvent, useCallback, useState } from "react";
 import type {
-  FieldValues,
-  Path,
-  PathValue,
-  UseFormReturn,
+    FieldValues,
+    Path,
+    PathValue,
+    UseFormReturn,
 } from "react-hook-form";
 
-export default function useUploadFile<T extends FieldValues>(
-  form: UseFormReturn<T>,
-) {
-  const [files, setFiles] = useState<Record<string, File> | null>(null);
-  const [preview, setPreview] = useState<Record<string, string> | null>(null);
+export default function useUploadFile<T extends FieldValues>(form: UseFormReturn<T>) {
+    const [files, setFiles] = useState<Record<string, File> | null>(null);
+    const [preview, setPreview] = useState<Record<string, string> | null>(null);
 
-  const handleFileChange = useCallback(
-    (field: Path<T>, file: File) => {
-      if (file) {
-        setFiles((prev) => ({
-          ...prev!,
-          [field]: file,
-        }));
+    const handleFileChange = useCallback(
+        (field: Path<T>, file: File) => {
+            if (file) {
+                setFiles((prev) => ({ ...prev!, [field]: file }));
 
-        form.setValue(field, file as PathValue<T, Path<T>>, {
-          shouldDirty: true,
-        });
-      }
-    },
-    [form],
-  );
-
-  const handlePreviewChange = useCallback(
-    async (field: Path<T>, result: string | ArrayBuffer | null, file: File) => {
-      setPreview((prev) => ({
-        ...prev!,
-        [field]: result as string,
-      }));
-      form.setValue(field, result as PathValue<T, Path<T>>, {
-        shouldDirty: true,
-      });
-    },
-    [form],
-  );
-
-  const handleChange = useCallback(
-    async (e: ChangeEvent<HTMLInputElement>, field: Path<T>) => {
-      if (!e.target.files) return;
-
-      const file = e.target.files[0];
-
-      try {
-        if (field !== "audio") {
-          handleFileChange(field, file);
-
-          const reader = new FileReader();
-          reader.onload = (event) => {
-            if (event.target?.result) {
-              handlePreviewChange(field, event.target?.result, file);
+                form.setValue(field, file as PathValue<T, Path<T>>, { shouldDirty: true });
             }
-          };
-          reader.readAsDataURL(file);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    [handleFileChange, handlePreviewChange],
-  );
+        },
+        [form],
+    );
 
-  return {
-    files,
-    setFiles,
-    preview,
-    setPreview,
-    handleChange,
-    handleFileChange,
-  };
+    const handlePreviewChange = useCallback(
+        async (field: Path<T>, result: string | ArrayBuffer | null) => {
+            setPreview((prev) => ({ ...prev!, [field]: result as string }));
+            form.setValue(field, result as PathValue<T, Path<T>>, { shouldDirty: true });
+        },
+        [form],
+    );
+
+    const handleChange = useCallback(
+        async (e: ChangeEvent<HTMLInputElement>, field: Path<T>) => {
+            if (!e.target.files) return;
+
+            const file = e.target.files[0];
+
+            try {
+                if (field !== "audio") {
+                    handleFileChange(field, file);
+
+                    const reader = new FileReader();
+                    reader.onload = (event) => {
+                        if (event.target?.result) { handlePreviewChange(field, event.target?.result) }
+                    };
+                    reader.readAsDataURL(file);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        [handleFileChange, handlePreviewChange],
+    );
+
+    return {
+        files,
+        setFiles,
+        preview,
+        setPreview,
+        handleChange,
+        handleFileChange,
+    };
 }
