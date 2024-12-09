1b8006d760c85c716d0ef73e13ca729b91742b07
wirayuda <wirayuda233@gmail.com>
2024-12-10 03:31:45 +0800

    add image confg

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
         pathname: '/**/*',
         port:''
       },
+{
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: '/**/*',
+        port:''
+      }
     ],
   },
   
